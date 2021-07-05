deae8e83ab8bacd4dc19c7a4c8983d719269ee50
Andrii Pukshyn <andriy.pukshyn@gmail.com>
2021-07-05 17:49:45 +0300

    updated mixin

--- src/Item.ts
+++ src/Item.ts
@@ -2,43 +2,14 @@ import { PagesIterable } from './pages-iterable';
 import { Pages } from './pages';
 import { Page } from './page';
 
-export abstract class Item implements PagesIterable<Page> {
+export abstract class Item extends PagesIterable(Object){
 
     protected pages: Pages
 
     protected constructor(pages: Pages) {
+        super();
         this.pages = pages;
     }
 
     abstract toString(): string;
-
-    [Symbol.iterator]() {
-        let pointer = 0;
-        let pages = this.pages.pages;
-
-        const firstPart = this.toString();
-        let secondPart = ''
-
-        return {
-            next(): IteratorResult<Page> {
-                if (pointer < pages.length) {
-                    if (pages[pointer]) {
-                        secondPart = pages[pointer].toString();
-                        pages[pointer].toString = () => {
-                            return firstPart + ', ' + secondPart;
-                        }
-                    }
-                    return {
-                        done: false,
-                        value: pages[pointer++]
-                    }
-                } else {
-                    return {
-                        done: true,
-                        value: null
-                    }
-                }
-            }
-        }
-    }
 }
